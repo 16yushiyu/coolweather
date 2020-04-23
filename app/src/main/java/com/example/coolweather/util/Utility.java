c560603aa00c181849af1d06ff5a0fc84fd22870
yushiyu <1294459991@qq.com>
2020-04-24 02:14:16 +0800

    加入显示天气信息功能

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -5,7 +5,10 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -70,5 +73,17 @@ public class Utility {
             }
         }
         return false;
+    }
+    //将返回的JSON数据解析成Weather实体类
+    public static Weather handleWeatherResponse(String reponse){
+        try{
+            JSONObject jsonObject=new JSONObject(reponse);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
